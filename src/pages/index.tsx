7658a1e10ce7155c4052c01f8d75de9b3264db7d
jimmymk23 <jimkeseling@gmail.com>
2021-04-08 14:27:57 -0400

    added site meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,21 +5,22 @@ import Head from 'next/head';
 import HeadBanner from '../components/HeadBanner';
 import HomeHero from '../components/HomeHero';
 import AboutSection from '../components/AboutSection';
-// import ShowcaseSection from '../components/ShowcaseSection';
-// import SkillSection from '../components/SkillSection'
 
 const index = () => {
 	return (
 		<>
 			<Head>
 				<title>James Keseling</title>
+				<meta
+					name='description'
+					content="I'm a front-end web developer looking to make the web more
+					friendly, functional, and accessible."
+				/>
 			</Head>
 			<HeadBanner />
 			<main>
 				<HomeHero />
-				{/* <ShowcaseSection /> */}
 				<AboutSection />
-				{/* <SkillSection /> */}
 			</main>
 		</>
 	);
