39cf4a1a0c32ee2d2f2e22c5e8f1f27ca6674f49
jimmymk23 <jimkeseling@gmail.com>
2021-04-22 23:00:27 -0400

    Added og meta tags and preview image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,45 @@ const index = () => {
 	return (
 		<>
 			<Head>
-				<title>James Keseling</title>
+				{/* Primary Meta Tags */}
+				<title>James Keseling | Front-End Web Dev Portfolio</title>
+				<meta
+					name='title'
+					content='James Keseling | Front-End Web Dev Portfolio'
+				/>
 				<meta
 					name='description'
-					content="I'm a front-end web developer looking to make the web more
-					friendly, functional, and accessible."
+					content="I'm looking to make websites more friendly, functional, and accessible with modern web standards and technologies."
 				/>
+
+				{/* Open Graph / Facebook */}
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://jameskeseling.com/' />
+				<meta
+					property='og:title'
+					content='James Keseling | Front-End Web Dev Portfolio'
+				/>
+				<meta
+					property='og:description'
+					content="I'm looking to make websites more friendly, functional, and accessible with modern web standards and technologies."
+				/>
+				<meta property='og:image' content='/images/preview.jpg' />
+
+				{/* Twitter */}
+				<meta property='twitter:card' content='summary_large_image' />
+				<meta
+					property='twitter:url'
+					content='https://jameskeseling.com/'
+				/>
+				<meta
+					property='twitter:title'
+					content='James Keseling | Front-End Web Dev Portfolio'
+				/>
+				<meta
+					property='twitter:description'
+					content="I'm looking to make websites more friendly, functional, and accessible with modern web standards and technologies."
+				/>
+				<meta property='twitter:image' content='/images/preview.jpg' />
 			</Head>
 			<HeadBanner />
 			<main>
