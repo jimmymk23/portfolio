3cb6428d15dbabdd59c8bd76d4fdb9614c2362c2
jimmymk23 <jimkeseling@gmail.com>
2021-04-29 13:53:43 -0400

    Moved meta tags to separate component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,58 +2,26 @@
 
 // IMPORT COMPONENTS
 import Head from 'next/head';
+import Meta from '../components/Meta';
 import HeadBanner from '../components/HeadBanner';
 import HomeHero from '../components/HomeHero';
-import AboutSection from '../components/AboutSection';
+import About from '../sections/About';
 
 const index = () => {
 	return (
 		<>
 			<Head>
-				{/* Primary Meta Tags */}
-				<title>James Keseling | Front-End Web Dev Portfolio</title>
-				<meta
-					name='title'
-					content='James Keseling | Front-End Web Dev Portfolio'
+				<Meta
+					title='James Keseling | Front-End Web Dev Portfolio'
+					description="I'm looking to make websites more friendly, functional, and accessible with modern web standards and technologies."
+					url='https://jameskeseling.com/'
+					image='/images/preview.jpg'
 				/>
-				<meta
-					name='description'
-					content="I'm looking to make websites more friendly, functional, and accessible with modern web standards and technologies."
-				/>
-
-				{/* Open Graph / Facebook */}
-				<meta property='og:type' content='website' />
-				<meta property='og:url' content='https://jameskeseling.com/' />
-				<meta
-					property='og:title'
-					content='James Keseling | Front-End Web Dev Portfolio'
-				/>
-				<meta
-					property='og:description'
-					content="I'm looking to make websites more friendly, functional, and accessible with modern web standards and technologies."
-				/>
-				<meta property='og:image' content='/images/preview.jpg' />
-
-				{/* Twitter */}
-				<meta property='twitter:card' content='summary_large_image' />
-				<meta
-					property='twitter:url'
-					content='https://jameskeseling.com/'
-				/>
-				<meta
-					property='twitter:title'
-					content='James Keseling | Front-End Web Dev Portfolio'
-				/>
-				<meta
-					property='twitter:description'
-					content="I'm looking to make websites more friendly, functional, and accessible with modern web standards and technologies."
-				/>
-				<meta property='twitter:image' content='/images/preview.jpg' />
 			</Head>
 			<HeadBanner />
 			<main>
 				<HomeHero />
-				<AboutSection />
+				<About />
 			</main>
 		</>
 	);
