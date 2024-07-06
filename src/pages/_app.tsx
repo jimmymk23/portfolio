32dd3d2ce1d158c2fa2879941e3865b432d3f0ac
James Keseling <jimkeseling@gmail.com>
2024-07-06 14:23:21 -0400

    Remove all old proj files

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps /*, AppContext */ } from 'next/app';
-import 'normalize.css';
-import '../styles/main.sass';
-
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
-
-export default MyApp;
